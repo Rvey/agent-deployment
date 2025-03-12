525808a27e3f64d59f83505012eeef6bbf5870ad
Redouane Bouabana <rredouane342@gmail.com>
2025-03-12 23:11:51 +0000

    add project files

---
+++ python-agent-example-app/main.py
@@ -0,0 +1,18 @@
+import logging
+
+from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, WorkerType, cli
+from dotenv import load_dotenv
+
+load_dotenv()
+
+logger = logging.getLogger("my-worker")
+logger.setLevel(logging.INFO)
+
+async def entrypoint(ctx: JobContext):
+    logger.info("starting entrypoint")
+    await ctx.connect(auto_subscribe=AutoSubscribe.SUBSCRIBE_ALL)
+    logger.info("connected to the room")
+
+
+if __name__ == "__main__":
+    cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint, worker_type=WorkerType.ROOM))
