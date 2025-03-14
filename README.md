62c8af7356321c15c018e0b091b9eb804ee2a0de
Redouane Bouabana <rredouane342@gmail.com>
2025-03-14 18:46:16 +0000

    update README handling; bump image version in render.yaml; add new dependencies in requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # agent-deployment
+
+## Building the Docker Image
+
+To build the Docker image, run the following command in the root directory of the project:
+
+```sh
+docker build -t your-dockerhub-username/agent-deployment:latest .
+```
+
+## Pushing the Docker Image to Docker Hub
+### After building the Docker image, you can push it to Docker Hub with the following commands:
+
+1. Log in to Docker Hub:
+
+```sh
+docker login
+```
+
+## Push the Docker image to Docker Hub:
+
+```sh
+docker push your-dockerhub-username/agent-deployment:latest
+```
+
+## Activating Virtual Environment and Installing Requirements
+To set up the Python virtual environment and install the required packages, follow these steps:
+
+1. Create a virtual environment:
+
+```sh
+python -m venv venv
+```
+
+2. Activate the virtual environment:
+
+On Windows:
+
+```sh
+venv\Scripts\activate
+```
+
+On macOS/Linux:
+
+```sh
+source venv/bin/activate
+```
+
+3. Install the required packages:
+
+```sh
+pip install -r requirements.txt
+```
--- main.py
+++ main.py
@@ -1,18 +1,135 @@
 import logging
 
-from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, WorkerType, cli
+from livekit.agents import AutoSubscribe, JobContext, WorkerOptions, WorkerType, cli, JobProcess
+
 from dotenv import load_dotenv
+
+from livekit.agents.pipeline import VoicePipelineAgent
+from livekit.plugins import openai, silero, turn_detector , elevenlabs
+import asyncio
+from livekit import rtc
+from livekit.agents import stt, transcription
+from livekit.agents import llm
 
 load_dotenv()
 
 logger = logging.getLogger("my-worker")
 logger.setLevel(logging.INFO)
 
+# async def entrypoint(ctx: JobContext):
+#     logger.info("starting entrypoint")
+#     await ctx.connect(auto_subscribe=AutoSubscribe.SUBSCRIBE_ALL)
+#     logger.info("connected to the room")
+
+def prewarm(proc: JobProcess):
+    proc.userdata["vad"] = silero.VAD.load()
+
+async def _forward_transcription(
+    stt_stream: stt.SpeechStream,
+    stt_forwarder: transcription.STTSegmentsForwarder,
+):
+    """Forward the transcription and log the transcript in the console"""
+    async for ev in stt_stream:
+        stt_forwarder.update(ev)
+        if ev.type == stt.SpeechEventType.INTERIM_TRANSCRIPT:
+            print(ev.alternatives[0].text, end="")
+        elif ev.type == stt.SpeechEventType.FINAL_TRANSCRIPT:
+            print("\n")
+            print(" -> ", ev.alternatives[0].text)
+
 async def entrypoint(ctx: JobContext):
-    logger.info("starting entrypoint")
-    await ctx.connect(auto_subscribe=AutoSubscribe.SUBSCRIBE_ALL)
-    logger.info("connected to the room")
+    initial_ctx = llm.ChatContext().append(
+        role="system",
+        text=(
+            """you are a helpful assistant"""
+        ),
+    )
+    # wake_word_handler = WakeWordHandler(
+    #     wake_word="Hey, showy!",
+    #     # notification_sound_path="path/to/sound.wav"
+    # )
+    stt = openai.STT()
+    tasks = []
+    async def transcribe_track(participant: rtc.RemoteParticipant, track: rtc.Track):
+        audio_stream = rtc.AudioStream(track)
+        stt_forwarder = transcription.STTSegmentsForwarder(
+            room=ctx.room, participant=participant, track=track
+        )
+        stt_stream = stt.stream()
+        stt_task = asyncio.create_task(
+            _forward_transcription(stt_stream, stt_forwarder)
+        )
+        print(f"Transcription: {stt_task}")
+        tasks.append(stt_task)
+
+        async for ev in audio_stream:
+            stt_stream.push_frame(ev.frame)
+
+    @ctx.room.on("track_subscribed")
+    def on_track_subscribed(
+        track: rtc.Track,
+        publication: rtc.TrackPublication,
+        participant: rtc.RemoteParticipant,
+    ):
+        if track.kind == rtc.TrackKind.KIND_AUDIO:
+            tasks.append(asyncio.create_task(transcribe_track(participant, track)))
+        logger.error("subscribed to track")
+    
+    # def _before_tts_cb(agent: VoicePipelineAgent, text: str | AsyncIterable[str]):
+    # # The TTS is incorrectly pronouncing "LiveKit", so we'll replace it with a phonetic
+    # # spelling
+    #     return text.replace("LiveKit", "Live Kit")
+    #     llm = openai.LLM(model="gpt-4o")
+    await ctx.connect(auto_subscribe=AutoSubscribe.AUDIO_ONLY)
+  
+    # Wait for the first participant to connect
+    participant = await ctx.wait_for_participant()
+    participant_attributes = participant.attributes
+    logger.info(f"connecting to room {participant.attributes.get('room_name')}")
+
+    # initialize the AssistantFnc class and pass it to the VoicePipelineAgent
+    # func_ctx = AssistantFnc(ctx=ctx, participant=participant)
+    logger.info(f"starting voice assistant for participant {participant.identity}")
+    eleven_tts=elevenlabs.tts.TTS(
+        model="eleven_turbo_v2_5",
+        api_key="sk_783fc29124493029c3553a30dfc3446998f91de38e814118",
+        voice=elevenlabs.tts.Voice(
+            id="A9ATTqUUQ6GHu0coCz8t",
+            name="Hamid",
+            category="premade",
+            settings=elevenlabs.tts.VoiceSettings(
+                stability=0.71,
+                similarity_boost=0.5,
+                style=0.0,
+                use_speaker_boost=True
+            ),
+        ),
+        language=participant_attributes.get("language_tts"),
+        streaming_latency=3,
+        enable_ssml_parsing=False,
+        chunk_length_schedule=[80, 120, 200, 260],
+    )
+    # This project is configured to use Deepgram STT, OpenAI LLM and TTS plugins
+    # Other great providers exist like Cartesia and ElevenLabs
+    # Learn more and pick the best one for your app:
+    # https://docs.livekit.io/agents/plugins
+    agent = VoicePipelineAgent(
+        vad=ctx.proc.userdata["vad"],
+        stt=openai.STT(model="whisper-1", language=participant_attributes.get("language_stt")),
+        llm=openai.LLM(model="gpt-4o"),
+        tts=eleven_tts,
+        chat_ctx=initial_ctx,
+        # before_llm_cb=wake_word_handler.before_llm_callback,
+        # fnc_ctx=func_ctx,
+        # max_nested_fnc_calls=5,
+        turn_detector=turn_detector.EOUModel(),
+    )
+
+    agent.start(ctx.room, participant)
+
+    # The agent should be polite and greet the user when it joins :)
+    await agent.say(participant_attributes.get("intro"), allow_interruptions=True)
 
 
 if __name__ == "__main__":
-    cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint, worker_type=WorkerType.ROOM))
+    cli.run_app(WorkerOptions(entrypoint_fnc=entrypoint,prewarm_fnc=prewarm,worker_type=WorkerType.ROOM))
